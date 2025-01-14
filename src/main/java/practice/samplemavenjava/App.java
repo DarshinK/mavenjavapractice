ae4e982301f144e4dc8d14c332eae81e1debe2bf
DarshinK <kapperasndarshin@gmail.com>
2025-01-14 10:11:59 +0530

    Added

---
+++ src/main/java/practice/samplemavenjava/App.java
@@ -0,0 +1,13 @@
+package practice.samplemavenjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
